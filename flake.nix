e0efafde0f3e61548c7937a4737c7d9508092ca9
gratisvictory <gratisvictory.dev@proton.me>
2025-06-09 02:45:06 +0400

    ✨ feat(nix-wsl): upgrade flake.nix

--- flake.nix
+++ flake.nix
@@ -1,76 +1,94 @@
 {
   description = "NixOS-WSL @gratisvictory configuration";
-
   inputs = {
-    nix-current-system.url = "github:nix-systems/current-system";
     nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
     nixpkgs-stable.url = "github:nixos/nixpkgs/nixos-25.05";
     nixpkgs-24-11.url = "github:nixos/nixpkgs/nixos-24.11";
-    home-manager = {
-      url = "github:nix-community/home-manager";
-      inputs.nixpkgs.follows = "nixpkgs-unstable";
-    };
-    nix-ld = {
-      url = "github:Mic92/nix-ld";
-      inputs.nixpkgs.follows = "nixpkgs-unstable";
-    };
-    nixos-wsl = {
-      url = "github:nix-community/NixOS-WSL";
-      inputs.nixpkgs.follows = "nixpkgs-unstable";
-    };
+    home-manager.url = "github:nix-community/home-manager";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs-unstable";
+    nix-ld.url = "github:Mic92/nix-ld";
+    nix-ld.inputs.nixpkgs.follows = "nixpkgs-unstable";
+    nixos-wsl.url = "github:nix-community/NixOS-WSL";
+    nixos-wsl.inputs.nixpkgs.follows = "nixpkgs-unstable";
   };
 
   outputs = {
     self,
-    nix-current-system,
+    nixpkgs-unstable,
+    nixpkgs-stable,
+    nixpkgs-24-11,
+    home-manager,
+    nixos-wsl,
+    nix-ld,
     ...
-  } @ inputs: let
-    system = nix-current-system;
+  }: let
+    system = "x86_64-linux";
     username = "gratisvictory";
     hostname = "nixos";
-    stateVersion = inputs.nixpkgs-unstable.lib.trivial.release;
+    stateVersion = nixpkgs-unstable.lib.trivial.release;
 
-    mkPkgs = nixpkgs: {
+    # nixpkgs (unstable)
+    pkgsUnstable = import nixpkgs-unstable {
       inherit system;
       config.allowUnfree = true;
     };
 
-    pkgsUnstable = import inputs.nixpkgs-unstable (mkPkgs inputs.nixpkgs-unstable);
-    pkgsStable = import inputs.nixpkgs-stable (mkPkgs inputs.nixpkgs-stable);
-    pkgs24-11 = import inputs.nixpkgs-24-11 (mkPkgs inputs.nixpkgs-24-11);
+    # nixpkgs (stable)
+    pkgsStable = import nixpkgs-stable {
+      inherit system;
+      config.allowUnfree = true;
+    };
 
-    mkOverlay = name: pkgs: final: prev: {${name} = pkgs;};
+    # nixpkgs (24.11)
+    pkgs24-11 = import nixpkgs-24-11 {
+      inherit system;
+      config.allowUnfree = true;
+    };
+
+    # overlay (unstable)
+    overlay-unstable = final: prev: {
+      unstable = pkgsUnstable;
+    };
+
+    # overlay (stable)
+    overlay-stable = final: prev: {
+      stable = pkgsStable;
+    };
+
+    # overlay (24.11)
+    overlay-24-11 = final: prev: {
+      "24-11" = pkgs24-11;
+    };
   in {
-    nixosConfigurations.${hostname} = inputs.nixpkgs-unstable.lib.nixosSystem {
+    nixosConfigurations.${hostname} = nixpkgs-unstable.lib.nixosSystem {
       inherit system;
       specialArgs = {
         inherit username hostname stateVersion pkgsUnstable pkgsStable pkgs24-11;
-        inherit inputs;
+        flake-inputs = {
+          inherit nixpkgs-unstable nixpkgs-stable nixpkgs-24-11 home-manager nixos-wsl nix-ld;
+        };
       };
       modules = [
         ({
+          flake-inputs,
           lib,
           config,
           ...
         }: {
           nixpkgs.overlays = [
-            (mkOverlay "unstable" pkgsUnstable)
-            (mkOverlay "stable" pkgsStable)
-            (mkOverlay "24-11" pkgs24-11)
+            overlay-unstable
+            overlay-stable
+            overlay-24-11
           ];
-
           imports = [
-            inputs.nixos-wsl.nixosModules.wsl
-            inputs.nix-ld.nixosModules.nix-ld
+            flake-inputs.nixos-wsl.nixosModules.wsl
+            flake-inputs.nix-ld.nixosModules.nix-ld
             {
-              programs.nix-ld = {
-                dev.enable = true;
-                libraries = inputs.nix-ld.packages.${system}.default.libraries;
-              };
+              programs.nix-ld.dev.enable = true;
+              programs.nix-ld.libraries = flake-inputs.nix-ld.packages.${system}.default.libraries;
             }
             ./wsl/nixos/default.nix
-
-            inputs.home-manager.nixosModules.home-manager
+            flake-inputs.home-manager.nixosModules.home-manager
             {
               home-manager = {
                 useGlobalPkgs = true;
